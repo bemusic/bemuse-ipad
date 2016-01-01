5229b83f94d02dad8c07d52a343dc29764ad04ee
Thai Pangsakulyanont <dtinth@spacet.me>
2016-01-01 15:06:01 +0700

    Initial Commit

---
+++ Bemuse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bemuse
+//
+//  Created by Thai Pangsakulyanont on 2016-01-01.
+//  Copyright © 2016年 Thai Pangsakulyanont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
